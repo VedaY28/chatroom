ea4035d8da7eebe34f084b6d1235736d9cefe9ed
VedaY <veda.yakkali3@gmail.com>
2024-03-15 15:00:19 -0400

    Partially Finished Delete Function of Message

--- components/ListMessages.tsx
+++ components/ListMessages.tsx
@@ -2,23 +2,21 @@
 import { useMessage } from "@/lib/store/messages";
 import React from "react";
 import Message from "./Message";
+import { DeleteAlert } from "./MessageActions";
 
 export default function ListMessages() {
+  const messages = useMessage((state) => state.messages);
 
-    const messages = useMessage((state)=>state.messages)
+  return (
+    <div className="flex-1 flex flex-col p-5 h-full overflow-y-auto">
+      <div className="flex-1"></div>
 
-
-    return (
-        <div className="flex-1 flex flex-col p-5 h-full overflow-y-auto">
-        <div className="flex-1"></div>
-
-        <div className=" space-y-7">
-            {messages.map((value, index) => {
-            return (
-                <Message key={index} message={value}/>
-            );
-            })}
-        </div>
-        </div>
-    );
+      <div className=" space-y-7">
+        {messages.map((value, index) => {
+          return <Message key={index} message={value} />;
+        })}
+      </div>
+      <DeleteAlert />
+    </div>
+  );
 }
--- lib/store/messages.ts
+++ lib/store/messages.ts
@@ -1,26 +1,31 @@
-import { User } from '@supabase/supabase-js'
-import { create } from 'zustand'
+import { User } from "@supabase/supabase-js";
+import { create } from "zustand";
 
 export type Imessage = {
+  created_at: string;
+  id: string;
+  is_edit: boolean;
+  send_by: string;
+  text: string;
+  users: {
+    avatar_url: string;
     created_at: string;
+    display_name: string;
     id: string;
-    is_edit: boolean;
-    send_by: string;
-    text: string;
-    users: {
-        avatar_url: string;
-        created_at: string;
-        display_name: string;
-        id: string;
-    } | null;
-}
+  } | null;
+};
 
 interface MessageState {
   messages: Imessage[];
+  actionMessage: Imessage | undefined;
   addMessage: (message: Imessage) => void;
+  setActionMessage: (message: Imessage | undefined) => void;
 }
 
 export const useMessage = create<MessageState>()((set) => ({
-    messages: [],
-    addMessage:(message)=>set((state)=>({messages:[...state.messages, message]}))
+  messages: [],
+  actionMessage: undefined,
+  addMessage: (message) =>
+    set((state) => ({ messages: [...state.messages, message] })),
+  setActionMessage: (message) => set(() => ({ actionMessage: message })),
 }));
