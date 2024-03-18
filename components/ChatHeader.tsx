15ef98b62bdceea6b9b9efaa9dc38fd134461e38
VedaY <veda.yakkali3@gmail.com>
2024-03-18 18:26:08 -0400

    Completed Implementing Current Online Tally

--- components/ChatHeader.tsx
+++ components/ChatHeader.tsx
@@ -1,43 +1,43 @@
 "use client";
-import React from 'react'
-import { Button } from './ui/button'
-import { supabaseBrowser } from '@/lib/supabase/browser';
-import { User } from '@supabase/supabase-js';
-import { useRouter } from 'next/navigation';
+import React from "react";
+import { Button } from "./ui/button";
+import { supabaseBrowser } from "@/lib/supabase/browser";
+import { User } from "@supabase/supabase-js";
+import { useRouter } from "next/navigation";
+import ChatPresence from "./ChatPresence";
 
-export default function ChatHeader({user}: {user:User | undefined}) {
-    const router = useRouter();
+export default function ChatHeader({ user }: { user: User | undefined }) {
+  const router = useRouter();
 
-    const handleLoginWithGithub = () => {
+  const handleLoginWithGithub = () => {
+    const supabase = supabaseBrowser();
+    supabase.auth.signInWithOAuth({
+      provider: "github",
+      options: {
+        redirectTo: location.origin + "/auth/callback",
+      },
+    });
+  };
 
-        const supabase = supabaseBrowser()
-        supabase.auth.signInWithOAuth({
-            provider:"github",
-            options:{
-                redirectTo: location.origin + "/auth/callback",
-            },
-        });
-    }
+  const handleLogout = async () => {
+    const supabase = supabaseBrowser();
+    await supabase.auth.signOut();
+    router.refresh();
+  };
 
-    const handleLogout =async () => {
-        const supabase = supabaseBrowser();
-        await supabase.auth.signOut();
-        router.refresh();
-    };
-
-    return (
-        <div className='h-20'>
-            <div className='p-5 border-b flex items-center justify-between h-full'>
-                <div>
-                    <h1 className='text-xl font-bold'>Daily Chat</h1>
-
-                    <div className='flex items-center gap-1'>
-                        <div className='h-4 w-4 bg-green-500 rounded-full animate animate-pulse'></div>
-                        <h1 className='text-sm text-gray-400'>2 Online</h1>
-                    </div>
-                </div>
-                {user?(<Button onClick={handleLogout}>Logout</Button>):(<Button onClick={handleLoginWithGithub}>Login</Button>)}
-            </div>
+  return (
+    <div className="h-20">
+      <div className="p-5 border-b flex items-center justify-between h-full">
+        <div>
+          <h1 className="text-xl font-bold">Daily Chat</h1>
+          <ChatPresence />
         </div>
-    );
+        {user ? (
+          <Button onClick={handleLogout}>Logout</Button>
+        ) : (
+          <Button onClick={handleLoginWithGithub}>Login</Button>
+        )}
+      </div>
+    </div>
+  );
 }
